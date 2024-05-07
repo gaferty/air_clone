a5c30995f91834c563add712ebd91c50db6bcd8e
lawrence harlow <lawrenceharlow@gmail.com>
2024-05-07 16:24:19 +0100

    created the login logic applied it to the bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,7 @@
 class BookingsController < ApplicationController
 
   def index
-    @bookings= Booking.where(user_id: 2)
+    @bookings= Booking.where(user_id: current_user.id)
   end
 
   def show
